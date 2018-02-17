5e114a178a555cec7593fa460411cfe5852b854a
Andrey Sibirev <kobolog@dropbox.com>
2018-02-17 04:52:12 +0000

    [core] explicitly ban ipv4 fragmentation.

--- lib/ip4.h
+++ lib/ip4.h
@@ -5,6 +5,9 @@
 #include <linux/in.h>
 #include <linux/ip.h>
 
+#define IP_MF 0x2000
+#define IP_OFFMASK 0x1FFF
+
 static BPF_INLINE int parse_ip4(void *ptr, void *end, uint32_t *src, uint32_t *dst, uint64_t *off)
 {
 	struct iphdr *hdr = ptr;
@@ -17,6 +20,10 @@ static BPF_INLINE int parse_ip4(void *ptr, void *end, uint32_t *src, uint32_t *d
 	*dst = hdr->daddr;
 	*off = hdr->ihl << 2;
 
+	// Explicitly ban fragmentation.
+	if (hdr->frag_off & IP_MF || (hdr->frag_off & IP_OFFMASK) != 0) {
+		return -1;
+	}
 
 	return hdr->protocol;
 }
